4a8300ce85e4251e141d7bb3b6d187463feb160c
aachalmakode <aachalmakode900@gmail.com>
2021-06-29 08:01:54 -0400

    Added

---
+++ src/Maximum.java
@@ -0,0 +1,32 @@
+public class Maximum {
+
+    public static void main(String[] args) {
+        Integer a = 2, b = 4, c = 6;
+        Float x = 3.4f, y = 4.6f, z = 6.7f;
+        findMaximum(a, b, c);
+
+        findMaximum(x, y, z);
+    }
+//uc1
+    private static void findMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(a) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println("Integer" + max);
+    }
+//uc2
+    private static void findMaximum(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(a) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println("Float" + max);
+    }
+}
