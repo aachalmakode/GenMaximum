dfb9fc36794a22c636c8314215c40b3ae10f8331
aachalmakode <aachalmakode900@gmail.com>
2021-06-29 22:09:38 -0400

    String max

--- src/com/generic/Maximum.java
+++ src/com/generic/Maximum.java
@@ -5,19 +5,19 @@ public class Maximum {
         Integer a = 2, b = 4, c = 6;
         Float x = 3.4f, y = 4.6f, z = 6.7f;
         String l = "apple", m = "banana", n = "orange";
-        findMaximum(x, y, z);
+        findMaximum(l, m, n);
 
     }
 
     //uc2
-    private static void findMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(a) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        System.out.println("Float" + max);
+    private static void findMaximum(String a , String b , String c){
+        String max = a;
+        if (b.campareTo(a) > 0);
+        max = b;
     }
+        if (c.compareTo(max) > 0) {
+        max = c;
+    }
+        System.out.println("String" + max);
+}
     }
