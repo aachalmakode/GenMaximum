618d935a5b6aea14c138e6fff141c2474eeacabe
aachalmakode <aachalmakode900@gmail.com>
2021-06-29 20:34:39 -0400

    String_Max

--- src/Maximum.java
+++ src/Maximum.java
@@ -3,9 +3,10 @@ public class Maximum {
     public static void main(String[] args) {
         Integer a = 2, b = 4, c = 6;
         Float x = 3.4f, y = 4.6f, z = 6.7f;
-        
+        String l = "apple", m = "banana", n = "orange";
         findMaximum(a, b, c);
         findMaximum(x, y, z);
+        findMaximum(l, m, n);
     }
 
     //uc1
@@ -31,13 +32,14 @@ public class Maximum {
         }
         System.out.println("Float" + max);
     }
-
-    public String maxPositionString(String firstStringPosition, String secondStringPosition, String thirdStringPosition) {
-        String maxString = firstStringPosition;
-        if (secondStringPosition.compareTo(maxString) > 0)
-            maxString = secondStringPosition;
-        if (thirdStringPosition.compareTo(maxString) > 0)
-            maxString = thirdStringPosition;
-        return maxString;
+    //uc3
+    private static void findMaximum(String a , String b , String c){
+        String max = a;
+        if (b.campareTo(a) > 0);
+        max = b;
     }
+        if (c.compareTo(max) > 0) {
+        max = c;
+    }
+        System.out.println("String" + max);
 }
